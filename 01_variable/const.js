003295f94574133e094d76344eee3b9eca9eb049
Besum1uve <aninditabanerjee0023@gmail.com>
2025-06-26 16:08:57 +0000

    container config done

---
+++ 01_variable/const.js
@@ -0,0 +1,8 @@
+const AccountName=123456
+
+let AccountPassword="121212"
+var AccountEmail="xyz@gmail.com"
+AccountCity="jaipur"
+
+console.log(AccountName)
+console.table([AccountPassword,AccountEmail,AccountCity])
