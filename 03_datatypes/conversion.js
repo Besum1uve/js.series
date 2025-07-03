928c033dc6440fe231c09a5decf36d57d4784f4e
Besum1uve <aninditabanerjee0023@gmail.com>
2025-07-03 14:21:02 +0000

    conversion done

---
+++ 03_datatypes/conversion.js
@@ -0,0 +1,17 @@
+let score=33
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let Score="33"
+console.log(typeof Score)
+let ValueInNumber=Number(Score)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+
+let isLoggedIn= "hello" 
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
