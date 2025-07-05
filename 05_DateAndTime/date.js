c51956eabcdf3a81b06a77b3378e79fa06b992a0
Besum1uve <aninditabanerjee0023@gmail.com>
2025-07-05 05:59:53 +0000

    date and time

---
+++ 05_DateAndTime/date.js
@@ -0,0 +1,12 @@
+const myDate=new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+console.log(myDate.toJSON());
+console.log(myDate.getTimezoneOffset());
